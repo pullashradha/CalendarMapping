c38005be42f4872b99fa99abdf47666d603faed0
Shradha Pulla <pullashradha@gmail.com>
2016-09-16 11:03:17 -0600

    ERROR FIXED: fixed password confirmation error by removing [Required] statement from ConfirmPassword property in RegisterViewModel & by adding info into the BeginForm method in Register view

--- src/CalendarMapping/ViewModels/RegisterViewModel.cs
+++ src/CalendarMapping/ViewModels/RegisterViewModel.cs
@@ -17,7 +17,6 @@ namespace CalendarMapping.ViewModels
         [Display (Name = "Password")]
         public string Password { get; set; }
 
-        [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Confirm Password")]
         [Compare("Password", ErrorMessage = "The passwords do not match!")]
