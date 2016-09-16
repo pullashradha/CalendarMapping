23cd0d4f266140eef286a540f2857f95b3ab4f54
Shradha Pulla <pullashradha@gmail.com>
2016-09-16 13:06:15 -0600

    Create RoleController & Role Index view to add authentication access for admins

---
+++ src/CalendarMapping/Controllers/RoleController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using CalendarMapping.Models;
+using CalendarMapping.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CalendarMapping.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RoleController (RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager, ApplicationDbContext db)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var rolesList = _db.Roles.ToList();
+            return View(rolesList);
+        }
+    }
+}
