b95bf8f6b09c63d808aff3ae0b8f9912765ada8e
Shradha Pulla <pullashradha@gmail.com>
2016-09-25 14:49:29 -0700

    Create tests file, pass first preliminary unit test of an event description

---
+++ CalendarMapping.Tests/EventTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CalendarMapping.Models;
+using Xunit;
+
+namespace CalendarMapping.Tests
+{
+    public class EventTest
+    {
+        [Fact]
+        public void GetDescription_Event_Test()
+        {
+            var newEvent = new Event();
+            newEvent.Description = "OHS Volunteer Party";
+            var resultDescription = newEvent.Description;
+            Assert.Equal("OHS Volunteer Party", resultDescription);
+        }
+    }
+}
---
+++ CalendarMapping.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalendarMapping.Tests")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3a496703-851c-4ad3-bed1-41e7a2b42a64")]
