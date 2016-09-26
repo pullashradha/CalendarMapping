3759a7972f358ebe54af51336144d05e9a293bc5
Shradha Pulla <pullashradha@gmail.com>
2016-09-26 15:36:52 -0700

    Create UserScripts.js file in Content folder, transfer all User view scripts code to UserScripts.js, since url property of AJAX cannot function outside a cshtml file, added @Html.Hidden tags to User views to reference AJAX requests to methods in UserController

---
+++ src/CalendarMapping/wwwroot/Content/js/UserScripts.js
@@ -0,0 +1,72 @@
+﻿$(document).ready(function () {
+//Index View
+    $("#log-out").click(function () {
+        $.ajax({
+            type: "POST",
+            dataType: "html",
+            data: $(this).serialize(),
+            url: $("#LogoutUrl").val(),
+            success: function (result) {
+                location.reload();
+            }
+        });
+    });
+//List View
+    $(".delete-user").submit(function (event) {
+        event.preventDefault();
+        if (confirm("Are you sure you want to delete this user?")) {
+            $.ajax({
+                type: "POST",
+                dataType: "html",
+                data: $(this).serialize(),
+                url: $("#DeleteUserUrl").val(),
+                success: function (result) {
+                    location.reload();
+                }
+            });
+        }
+    });
+//Profile View
+    $(".edit-user").hide();
+    $("#edit-user-btn").click(function () {
+        $(".edit-user").toggle();
+    });
+    $("#edit-user-form").submit(function (event) {
+        event.preventDefault();
+        $.ajax({
+            type: "POST",
+            dataType: "html",
+            data: $(this).serialize(),
+            url: $("#EditUrl").val(),
+            success: function (result) {
+                location.reload();
+            }
+        });
+    });
+    $("#edit-username").submit(function (event) {
+        event.preventDefault();
+        $.ajax({
+            type: "POST",
+            dataType: "html",
+            data: $(this).serialize(),
+            url: $("#EditUsernameUrl").val(),
+            success: function (result) {
+                location.reload();
+            }
+        });
+    });
+    $("#delete-user").submit(function (event) {
+        event.preventDefault();
+        if (confirm("Are you sure you want to delete your account?")) {
+            $.ajax({
+                type: "POST",
+                dataType: "html",
+                data: $(this).serialize(),
+                url: $("#DeleteCurrentUserUrl").val(),
+                success: function (result) {
+                    location.reload();
+                }
+            });
+        }
+    });
+});
