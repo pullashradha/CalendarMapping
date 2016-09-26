ab7eec372d9283f3a935cd30c938e404ec60ac60
Shradha Pulla <pullashradha@gmail.com>
2016-09-26 15:42:57 -0700

    Create RoleScripts.js file & transfer all scripts code from Role view to that file, add @Html.Hidden tags to Role views

---
+++ src/CalendarMapping/wwwroot/Content/js/RoleScripts.js
@@ -0,0 +1,57 @@
+﻿$(document).ready(function () {
+//Index
+    $("#new-role-form").hide();
+    $("#new-role-btn").click(function () {
+        $("#new-role-form").toggle();
+    });
+    $("#new-role-form").submit(function (event) {
+        event.preventDefault();
+        $.ajax({
+            type: "POST",
+            dataType: "html",
+            data: $(this).serialize(),
+            url: $("#CreateUrl").val(),
+            success: function (result) {
+                location.reload();
+            }
+        });
+    });
+    $(".edit-role").submit(function (event) {
+        event.preventDefault();
+        $.ajax({
+            type: "POST",
+            dataType: "html",
+            data: $(this).serialize(),
+            url: $("#EditUrl").val(),
+            success: function (result) {
+                location.reload();
+            }
+        });
+    });
+    $(".delete-role").submit(function (event) {
+        event.preventDefault();
+        if (confirm("Are you sure you want to delete this role?")) {
+            $.ajax({
+                type: "POST",
+                dataType: "html",
+                data: $(this).serialize(),
+                url: $("#DeleteUrl").val(),
+                success: function (result) {
+                    location.reload();
+                }
+            });
+        }
+    });
+    $("#add-user-form").submit(function (event) {
+        event.preventDefault();
+        $.ajax({
+            type: "POST",
+            dataType: "html",
+            data: $(this).serialize(),
+            url: $("#AddUserUrl").val(),
+            success: function (result) {
+                location.reload();
+            }
+        });
+    });
+});
