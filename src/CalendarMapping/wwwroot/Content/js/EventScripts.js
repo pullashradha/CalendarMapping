24ddc19121dee23caf98c0ccfc89764355816182
Shradha Pulla <pullashradha@gmail.com>
2016-09-26 15:48:06 -0700

    Create EventScripts.js file and transfer all Event view ajax requests to it, add @Html.Hidden tags to Event views

--- src/CalendarMapping/wwwroot/Content/js/EventScripts.js
+++ src/CalendarMapping/wwwroot/Content/js/EventScripts.js
@@ -1,1 +1,64 @@
-﻿
+﻿$(document).ready(function () {
+//Index
+    $("#new-event-form").hide();
+    $("#new-event-btn").click(function () {
+        $("#new-event-form").toggle();
+    });
+    $("#new-event-form").submit(function (event) {
+        event.preventDefault();
+        $.ajax({
+            type: "POST",
+            dataType: "html",
+            data: $(this).serialize(),
+            url: $("#CreateUrl").val(),
+            success: function (result) {
+                location.reload();
+            }
+        });
+    });
+    $(".delete-event").submit(function (event) {
+        event.preventDefault();
+        if (confirm("Are you sure you want to delete this event?")) {
+            $.ajax({
+                type: "POST",
+                dataType: "html",
+                data: $(this).serialize(),
+                url: $("#DeleteUrl").val(),
+                success: function (result) {
+                    location.reload();
+                }
+            });
+        }
+    });
+//Detail
+    $("#edit-event").hide();
+    $("#edit-form-btn").click(function () {
+        $("#edit-event").toggle();
+    });
+    $("#edit-event").submit(function (event) {
+        event.preventDefault();
+        $.ajax({
+            type: "POST",
+            dataType: "html",
+            data: $(this).serialize(),
+            url: $("#EditUrl").val(),
+            success: function (result) {
+                location.reload();
+            }
+        });
+    });
+    $(".delete-event").submit(function (event) {
+        event.preventDefault();
+        if (confirm("Are you sure you want to delete this event?")) {
+            $.ajax({
+                type: "POST",
+                dataType: "html",
+                data: $(this).serialize(),
+                url: $("#DeleteCurrentEventUrl").val(),
+                success: function (result) {
+                    location.reload();
+                }
+            });
+        }
+    });
+});
